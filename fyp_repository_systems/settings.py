782531573077cf45fd8a594948e4b715016058a9
malikasim001 <124617951+malikasim001@users.noreply.github.com>
2023-02-07 13:29:20 +0500

    Update settings.py

--- fyp_repository_systems/settings.py
+++ fyp_repository_systems/settings.py
@@ -136,7 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 AUTH_USER_MODEL="fyp_repository_app.CustomUser"
 AUTHENTICATION_BACKENDS=['fyp_repository_app.EmailBackEnd.EmailBackEnd']
